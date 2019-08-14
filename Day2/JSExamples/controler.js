d01b8c8da40f63eb136fa1795304868edec9bd00
dkossally <39966675+dkossally@users.noreply.github.com>
2019-08-14 10:50:13 -0500

    day 2 updates

--- Day2/JSExamples/controler.js
+++ Day2/JSExamples/controler.js
@@ -1,13 +1,39 @@
 
+function alertMe(){
+    alert('Clicked the button');
+}
+
+function alertMe2(){
+    alert('Alert me again');
+}
+
+//let button = document.querySelector('div');
+
+//button.addEventListener("click", alertMe());
+//button.addEventListener("click", alertMe2());
+
+function logme(event){
+    console.log(event.target.value);
+}
 
 //ARROW FUNCTIONS
 function Counter() {
+    'use strict';
+    console.log(this);
     this.num = 0;
     this.timer = setInterval(function add() {
         this.num++;
         console.log(this.num);
         console.log(this);
     }, 1000);
+}
+
+
+
+let arrowfunc = (arg1, arg2, ...arguments) => {
+    console.log(arg1);
+    console.log(arg2);
+    console.log(arguments);
 }
 
 function CounterArrow() {
@@ -18,3 +44,34 @@ function CounterArrow() {
         console.log(this);
     }, 1000);
 }
+
+let user = {
+    firstName: "John",
+    sayHi() {
+        alert(`Hello, ${this.firstName}!`);
+    }
+};
+
+
+
+
+let myFirstPromise = new Promise((resolve, reject) => {
+    // We call resolve(...) when what we were doing asynchronously was successful, and reject(...) when it failed.
+    // In this example, we use setTimeout(...) to simulate async code. 
+    // In reality, you will probably be using something like XHR or an HTML5 API.
+    setTimeout(function(){
+      //resolve("Success!"); // Yay! Everything went well!
+      reject("Error!");
+    }, 2000);
+  });
+  
+  myFirstPromise.then((successMessage) => {
+    // successMessage is whatever we passed in the resolve(...) function above.
+    // It doesn't have to be a string, but if it is only a succeed message, it probably will be.
+    console.log("Yay! " + successMessage);
+    this.result = successMessage;
+  })
+  .catch((error) => {
+        console.log("Oh No! " + error);
+      this.error = error;
+  });
